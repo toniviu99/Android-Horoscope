34289e20a2976e8afd7a05a57b07752f62356946
toniviu99 <toniviugutierrez@gmail.com>
2023-10-23 10:41:38 +0200

    initial commit

---
+++ app/src/main/java/com/example/horoscopoapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,26 @@
+package com.example.horoscopoapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.horoscopoapp.R
+import com.example.horoscopoapp.databinding.FragmentHoroscopeBinding
+import com.example.horoscopoapp.databinding.FragmentPalmistryBinding
+
+
+class PalmistryFragment : Fragment() {
+
+    private var _binding: FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
